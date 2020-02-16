a9285a0579e1d19965e091ee78bd70a7c5223e6d
Macbook <Macbook@MacBook-Pro-Macbook.local>
2020-02-16 17:11:46 +0200

    style(fixed style with prettier): fixed style with prettier and added properties with dotenv's help
    fixed style with prettier and added properties with dotenv's help

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
-const http = require("http");
-const events = require("./events");
-const server = require("./server");
-const port = server.get("port");
+const http = require('http');
+const events = require('./events');
+const server = require('./server');
+const port = server.get('port');
 
 events.bind(http.createServer(server).listen(port));
